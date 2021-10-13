51c369dc97e97a599acd669b24ab1955406db063
Denjse23 <90514338+Denjse23@users.noreply.github.com>
2021-10-13 15:11:41 +0800

    Update adxlint.ino

--- adxlint/adxlint.ino
+++ adxlint/adxlint.ino
@@ -1,112 +1,105 @@
 #include <SparkFun_ADXL345.h>
+#include <avr/wdt.h>
 
 #include <Wire.h>
-
+#define ledPin 13
+#define soundPin 8
+#define greenPin 6
+#define redPin 7
 
 /* Assign a unique ID to this sensor at the same time */
 ADXL345 adxl = ADXL345();
-const float g = 9.81;
-const float LFT = 0.5;
-const float UFT = 2.8;
-const float ST = 1.25;
+unsigned volatile long timeout = 0;
 
-bool fallFlag = false;
-bool stepFlag = false;
-bool alarmTrig = false;
-
-unsigned long fallTimer = 0;
-unsigned long alarmTimer = 0;
-unsigned long stepTimer = 0;
-unsigned long fsrTimer = 0;
-unsigned long printTimer = 0;
-
-const long fallInterval = 500;
-const long alarmInterval = 125;
-const long stepInterval = 100;
-const long fsrInterval = 40;
-const long printInterval = 200;
-
-const int ledPin =  LED_BUILTIN;
-int ledState = LOW;
-
-unsigned int stepCount = 0;
-int fsrprev = 0;
-
-void alarmOn() {
-//  if (millis() - alarmTimer >= alarmInterval) {
-//    // save the last time you blinked the LED
-//    alarmTimer = millis();
-//
-//    // if the LED is off turn it on and vice-versa:
-//    if (ledState == LOW) {
-//      ledState = HIGH;
-//    } else {
-//      ledState = LOW;
-//    }
-//
-//    // set the LED with the ledState of the variable:
-//    digitalWrite(ledPin, ledState);
-// }
-  digitalWrite(ledPin,HIGH);
-  delay(200);
-  digitalWrite(ledPin,LOW);
-  
+void showSetupComplete() {
+    for (int i = 0; i < 2; i++) {
+        digitalWrite(ledPin,HIGH);
+        digitalWrite(soundPin,HIGH);
+        digitalWrite(greenPin, HIGH);
+        delay(50);
+        digitalWrite(ledPin,LOW);
+        digitalWrite(soundPin,LOW);
+        digitalWrite(greenPin,LOW);
+        delay(50);
+    }
 }
 
-void printToSerial(double anet, int stepCount, bool alarmTrig) {
-  if (millis() - printTimer >= printInterval) {
-    printTimer = millis();
-  
-    /* Display the results (acceleration is measured in g) */
-    Serial.print("Net: "); Serial.print(anet); Serial.println(" g");
-  
-    // Print number of steps
-    Serial.print("Steps: "); Serial.println(stepCount);
-
-    if (alarmTrig) {
-      Serial.println("Fall detected!");
-    }
-
-    Serial.println(" ");
-  }
+void alarmOn() {
+     digitalWrite(ledPin,HIGH);
+     digitalWrite(soundPin,HIGH);
+     digitalWrite(redPin,HIGH);
+     delay(1000);
+     digitalWrite(ledPin,LOW); 
+    digitalWrite(soundPin,LOW);
+    digitalWrite(redPin,LOW);
 }
 
 void setup() {
   Serial.begin(9600);
+  //Setup builtin LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(soundPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin,OUTPUT);
+    setupADXL();
+
+}
+
+void setupADXL() {
   adxl.powerOn();
   adxl.setRangeSetting(4);
-  adxl.setRate(50);
+  //adxl.setRate(10);
   adxl.setActivityXYZ(1, 1, 1);
   adxl.setActivityThreshold(75);
   adxl.setInactivityXYZ(1, 0, 0);
   adxl.setInactivityThreshold(75);
-  adxl.setTimeInactivity(10);
+  adxl.setTimeInactivity(6);
   adxl.setTapDetectionOnXYZ(1, 1, 1);
   adxl.setTapThreshold(50);
   adxl.setTapDuration(15);
   adxl.setDoubleTapLatency(80);
   adxl.setDoubleTapWindow(200);
   adxl.setFreeFallThreshold(5);
-  adxl.setFreeFallDuration(15);
+  adxl.setFreeFallDuration(20);
   adxl.InactivityINT(1);
   adxl.ActivityINT(1);
   adxl.FreeFallINT(1);
   adxl.doubleTapINT(1);
   adxl.singleTapINT(1);
-  //Setup builtin LED
-  pinMode(LED_BUILTIN, OUTPUT);
+  wdt_enable(WDTO_2S);
+  showSetupComplete();
 }
 
 void loop() {
+  //alarmOn();
   int x,y,z;   
-  adxl.readAccel(&x, &y, &z); 
+  wdt_reset();
+//  adxl.readAccel(&x, &y, &z); 
+//  if (millis() > timeout) {
+//     Serial.print(x);
+//  Serial.print(", ");
+//  Serial.print(y);
+//  Serial.print(", ");
+//  Serial.println(z);
+//  timeout = millis() + 2000;
+//  alarmOn();
+//  }
   ADXL_ISR();
-  //if (adxl.error_code == ADXL345_READ_ERROR) alarmOn();
+//  delay(400);
+//  if (adxl.error_code == ADXL345_READ_ERROR) {
+//    digitalWrite(ledPin,HIGH);
+//    //timeout = millis(); + 3000;
+//  }
+
+//  if (millis() > timeout) {
+//    digitalWrite(ledPin,LOW);
+//  }
 //  Serial.print(x);
 //  Serial.print(", ");
 //  Serial.print(y);
 //  Serial.print(", ");
 //  Serial.println(z); 
+delay(100);
 }
 
 void ADXL_ISR() {
@@ -114,7 +107,6 @@ void ADXL_ISR() {
   // getInterruptSource clears all triggered actions after returning value
   // Do not call again until you need to recheck for triggered actions
   byte interrupts = adxl.getInterruptSource();
-  //if(adxl.status == ADXL345_READ_ERROR) ADXL_ISR();
   
   // Free Fall Detection
   if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
@@ -124,8 +116,8 @@ void ADXL_ISR() {
   } else if(adxl.triggered(interrupts, ADXL345_ACTIVITY)){
     //Serial.println("*** ACTIVITY ***"); 
      //add code here to do when activity is sensed
-  } else {
-    Serial.println("Nothing");
-  }
-  
+     //alarmOn();
+  } else if (adxl.triggered(interrupts, ADXL345_INACTIVITY)) {
+    //Serial.println("Nothing");
+  } 
 }
